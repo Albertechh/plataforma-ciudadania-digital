154695df80e0cb6ab2dd37a8f87eb3f3ca7e007d
Albertechh <luis@bridgepointschool.com.mx>
2025-04-09 07:30:14 +0000

    Agrego layout.tsx completo con estructura correcta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
-import "./globals.css";
+import "../app/global.css";
 import { ReactNode } from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "Ciudadanía Digital",
-  description: "Plataforma para preescolar a preparatoria",
+  description: "Una plataforma educativa desde preescolar hasta preparatoria",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="es">
+      <head />
       <body className="bg-white text-black">
         <Navbar />
         <main className="min-h-screen p-4">{children}</main>
